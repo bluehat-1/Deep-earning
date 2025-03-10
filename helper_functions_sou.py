f0100b6f429ba47fe76fba217ffaf313d7eb4765
bluehat-1 <127422390+bluehat-1@users.noreply.github.com>
2025-03-10 18:28:41 +0530

    Add files via upload

--- helper_functions_sou.py
+++ helper_functions_sou.py
@@ -336,3 +336,45 @@ def plot_loss_curves_refine(history):
   plt.legend()
 
 
+def compare_histories_refine(original_history, new_history, initial_epochs=5):
+    """
+    Compares two TensorFlow model History objects.
+    
+    Args:
+      original_history: History object from original model (before new_history)
+      new_history: History object from continued model training (after original_history)
+      initial_epochs: Number of epochs in original_history (new_history plot starts from here) 
+    """
+    
+    # Get original history measurements
+    acc = original_history["accuracy"]
+    loss = original_history["loss"]
+
+    val_acc = original_history["val_accuracy"]
+    val_loss = original_history["val_loss"]
+
+    # Combine original history with new history
+    total_acc = acc + new_history["accuracy"]
+    total_loss = loss + new_history["loss"]
+
+    total_val_acc = val_acc + new_history["val_accuracy"]
+    total_val_loss = val_loss + new_history["val_loss"]
+
+    # Make plots
+    plt.figure(figsize=(8, 8))
+    plt.plot(total_acc, label='Training Accuracy')
+    plt.plot(total_val_acc, label='Validation Accuracy')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='lower right')
+    plt.title('Training and Validation Accuracy')
+
+    plt.figure(figsize=(8, 8))
+    plt.plot(total_loss, label='Training Loss')
+    plt.plot(total_val_loss, label='Validation Loss')
+    plt.plot([initial_epochs-1, initial_epochs-1],
+              plt.ylim(), label='Start Fine Tuning') # reshift plot around epochs
+    plt.legend(loc='upper right')
+    plt.title('Training and Validation Loss')
+    plt.xlabel('epoch')
+    plt.show()
